be7f815e0b6b3a201027e4ddd7845f6fd52e9ed4
Carlos.Arroyoh <carlos.arroyoh@uanl.mxm>
2022-11-22 11:20:55 -0600

    Primer commit

---
+++ webpack.prod.js
@@ -0,0 +1,63 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    
+    mode: 'production',
+
+    output : {
+        clean: true,
+    },
+
+    module:{
+        rules:[
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false,
+                    minimize:false,
+                    
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /style.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /style.css$/,
+                use: [MiniCssExtractPlugin.loader , 'css-loader'],
+
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader',
+            }
+        ]
+    },
+
+    optimization: {}, 
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[hash].css',
+            ignoreOrder: false,
+
+        }),
+        new CopyPlugin({
+            patterns:[
+                { from: 'src/assets/', to: 'assets/'}
+            ],
+        }
+            
+        )
+        
+    ],
+
+}
